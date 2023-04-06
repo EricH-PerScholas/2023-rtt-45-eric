84ba9192c901d0d4bc27443d3b91bc9745deedad
Anjaom <eric@webworkbench.net>
2023-04-06 13:37:21 -0600

    added sessione example to index controller

--- SpringBoot/SpringBoot/src/main/java/springexamples/controller/SlashController.java
+++ SpringBoot/SpringBoot/src/main/java/springexamples/controller/SlashController.java
@@ -1,11 +1,13 @@
 package springexamples.controller;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
 import springexamples.database.dao.EmployeeDAO;
 
@@ -15,10 +17,12 @@ public class SlashController {
 
 
     @RequestMapping(value = { "/index", "/", "/index.html" }, method = RequestMethod.GET)
-    public ModelAndView index() {
+    public ModelAndView index(HttpSession session) {
 
         //log.debug("In the index controller method");
         ModelAndView response = new ModelAndView("index");
+
+        log.debug("################# SESSION ATTRIBUTE NAME = " + session.getAttribute("name"));
 
         log.trace("This is a trace log example");
         log.debug("This is a debug log example");
@@ -30,15 +34,22 @@ public class SlashController {
     }
 
     @GetMapping("/signup")
-    public ModelAndView setup() {
+    public ModelAndView setup(HttpSession session) {
         log.debug("In the signup controller method");
         ModelAndView response = new ModelAndView("signup");
+
+        log.debug("Signup method in the controller is setting a value in the session");
+        session.setAttribute("name", "signup set this value");
+
         return response;
     }
 
     @RequestMapping(value = "/bootstrap", method = RequestMethod.GET)
-    public ModelAndView bootstrapExample() {
+    public ModelAndView bootstrapExample(HttpSession session) {
         log.debug("In the bootstrap controller method");
+
+        log.debug("Bootstrap method in the controller is setting a value in the session");
+        session.setAttribute("name", "bootstrap set this value");
         ModelAndView response = new ModelAndView("bootstrap");
         return response;
     }
