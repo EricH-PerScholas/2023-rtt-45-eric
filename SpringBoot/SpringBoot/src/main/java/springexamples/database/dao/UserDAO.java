77490bdb3efbe0aee690ef7460556b8a63e0f07d
Anjaom <eric@webworkbench.net>
2023-04-11 10:05:58 -0600

    jsr303 validation

--- SpringBoot/SpringBoot/src/main/java/springexamples/database/dao/UserDAO.java
+++ SpringBoot/SpringBoot/src/main/java/springexamples/database/dao/UserDAO.java
@@ -11,4 +11,6 @@ public interface UserDAO extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
 
+    boolean existsByEmail(String email);
+
 }
--- SpringBoot/SpringBoot/src/main/java/springexamples/formbeans/EmployeeFormBean.java
+++ SpringBoot/SpringBoot/src/main/java/springexamples/formbeans/EmployeeFormBean.java
@@ -1,8 +1,14 @@
 package springexamples.formbeans;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+import springexamples.validation.EmailUnique;
 
 @Getter
 @Setter
@@ -12,10 +18,20 @@ public class EmployeeFormBean {
     // we add this as a way to pass the id to and from the controller using a hidden form input field
     private Integer id;
 
+    // @NotNull is valid if the value is not null or if the value is ""
+    // @NotEmpty is valid if the value is not null and if the value is not "" and if the value length > 0
+
+    @NotEmpty(message = "Email address is required.")
+    @Length(max = 20, message= "Email can not be longer than 20 characters")
+    @Pattern(regexp=".+@.+\\.[a-z]+", message = "Invalid email format")
+    @EmailUnique(message= "Email already exists in database")
     private String email;
 
+    @NotEmpty(message = "First name is required.")
+    @Length(max = 40, message= "First name can not be longer than 40 characters")
     private String firstName;
 
+    @NotEmpty(message = "Last name is required.")
     private String lastName;
 
     private String jobTitle;
@@ -24,6 +40,8 @@ public class EmployeeFormBean {
 
     private String profileImage;
 
+    @Max(value=100, message="You can not have more than 100 vacation hours")
+    @Min(value=0, message="You can not have less than 0 vacation hours")
     private Integer vacationHours;
 
     private Integer officeId;
